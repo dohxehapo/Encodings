d3452a6597b64403bf776f4353b3284a80816283
dohxehapo <dohxehapo@gmail.com>
2016-08-05 00:50:16 +0300

    - initial commit with base64 ready

--- Encodings_test/stdafx.h
+++ Encodings_test/stdafx.h
@@ -1,13 +1,10 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
-#include "targetver.h"
+#ifndef STDAFX_H
+#define STDAFX_H
 
-// Headers for CppUnitTest
+
 #include "CppUnitTest.h"
 
-// TODO: reference additional headers your program requires here
+
+#endif // STDAFX_H
---
+++ Include/Base64.h
@@ -0,0 +1,124 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Module:		Base64
+// Author:		Anton Egorov, dohxehapo@gmail.com
+// Description:	TODO:
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+#pragma once
+
+#ifndef ENCODINGS_BASE64_H
+#define ENCODINGS_BASE64_H
+
+
+#include <string>
+#include <vector>
+
+
+namespace Base64
+{
+
+	static std::string g_base64EncodeTable =
+		"ABCDEFGH"
+		"IJKLMNOP"
+		"QRSTUVWX"
+		"YZabcdef"
+		"ghijklmn"
+		"opqrstuv"
+		"wxyz0123"
+		"456789+/";
+
+	static std::vector<int> g_base64DecodeTable =
+	{
+		62, 0, 0, 0, 63, 52, 53, 54,
+		55, 56, 57, 58, 59, 60, 61, 0,
+		0, 0, 0, 0, 0, 0, 0, 1,
+		2, 3, 4, 5, 6, 7, 8, 9,
+		10, 11, 12, 13, 14, 15, 16, 17,
+		18, 19, 20, 21, 22, 23, 24, 25,
+		0, 0, 0, 0, 0, 0, 26, 27,
+		28, 29, 30, 31, 32, 33, 34, 35,
+		36, 37, 38, 39, 40, 41, 42, 43,
+		44, 45, 46, 47, 48, 49, 50, 51
+	};
+
+
+	static std::string Base64_Encode(const std::string &pInput)
+	{
+		// We will process 3 bytes at a time. The remainder will be appended in the end
+		int remainder = pInput.size() % 3;
+		int length3 = pInput.size() / 3;
+
+		// length3 * 4 because every 3 bytes are encoded by 4 base64 symbols. (length3 + 1) to reserve 4 chars for remainder encoding
+		int resLength = (remainder > 0) ? (length3 + 1) * 4 : length3 * 4;
+		std::string res;
+		res.reserve(resLength);
+
+		int i;
+		for (i = 0; i < length3; ++i)
+		{
+			res.push_back(g_base64EncodeTable[(pInput[i * 3 + 0] >> 2)]);
+			res.push_back(g_base64EncodeTable[((pInput[i * 3 + 0] & 0x03) << 4) | ((pInput[i * 3 + 1] & 0xF0) >> 4)]);
+			res.push_back(g_base64EncodeTable[((pInput[i * 3 + 1] & 0x0F) << 2) | ((pInput[i * 3 + 2] & 0xC0) >> 6)]);
+			res.push_back(g_base64EncodeTable[(pInput[i * 3 + 2] & 0x3F)]);
+		}
+
+		if (remainder == 1)
+		{
+			res.push_back(g_base64EncodeTable[(pInput[i * 3 + 0] >> 2)]);
+			res.push_back(g_base64EncodeTable[((pInput[i * 3 + 0] & 0x03) << 4)]);
+			res.push_back('=');
+			res.push_back('=');
+		}
+		else if (remainder == 2)
+		{
+			res.push_back(g_base64EncodeTable[(pInput[i * 3 + 0] >> 2)]);
+			res.push_back(g_base64EncodeTable[((pInput[i * 3 + 0] & 0x03) << 4) | ((pInput[i * 3 + 1] & 0xF0) >> 4)]);
+			res.push_back(g_base64EncodeTable[((pInput[i * 3 + 1] & 0x0F) << 2)]);
+			res.push_back('=');
+		}
+
+		return res;
+	}
+
+
+	static std::string Base64_Decode(const std::string &pInput)
+	{
+		// We will process 4 bytes at a time. Input base64 string must be multiple of 4
+		int length4 = pInput.size() / 4;
+
+		// length4 * 3 because every 3 original bytes are encoded by 4 base64 symbols
+		std::string res;
+		res.reserve(length4 * 3);
+
+		if (pInput[pInput.size() - 1] == '=')
+		{
+			// If we have at least one '=' in the end, then we should parse the last 4 symbols not in the main loop
+			--length4;
+		}
+
+		int i;
+		for (i = 0; i < length4; ++i)
+		{
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 0] - 43] << 2) | (g_base64DecodeTable[pInput[i * 4 + 1] - 43] >> 4));
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 1] - 43] << 4) | (g_base64DecodeTable[pInput[i * 4 + 2] - 43] >> 2));
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 2] - 43] << 6) | (g_base64DecodeTable[pInput[i * 4 + 3] - 43]));
+		}
+
+		if (pInput[pInput.size() - 2] == '=')
+		{
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 0] - 43] << 2) | (g_base64DecodeTable[pInput[i * 4 + 1] - 43] >> 4));
+		}
+		else if (pInput[pInput.size() - 1] == '=')
+		{
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 0] - 43] << 2) | (g_base64DecodeTable[pInput[i * 4 + 1] - 43] >> 4));
+			res.push_back((g_base64DecodeTable[pInput[i * 4 + 1] - 43] << 4) | (g_base64DecodeTable[pInput[i * 4 + 2] - 43] >> 2));
+		}
+
+		return res;
+	}
+
+} // ns Base64
+
+
+#endif // ENCODINGS_BASE64_H
